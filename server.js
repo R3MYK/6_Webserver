c40c64aa1da4509b6f9cb76d855b808a41dbdd71
Nicolas <nicolasmartinezgutierrez@hotmail.com>
2020-02-10 19:12:17 -0300

    Commit Pages and Boostrap
    Se agregan libreria de bootstrap y algunos componentes de este, se configuran middelware carpeta publica

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const app = express();
+// Middelware
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    // res.send('Hello World')
-    let salida = {
-        nombre: 'Nicolas',
-        edad: 31,
-        url: req.url
-    };
-    res.send(salida);
-});
-app.get('/data', function (req, res) {
-    res.send('Hello Data')
-    // let salida = {
-    //     nombre: 'Nicolas',
-    //     edad: 31,
-    //     url: req.url
-    // };
-    // res.send(salida);
-});
+// app.get('/', function (req, res) {
+//     // res.send('Hello World')
+//     let salida = {
+//         nombre: 'Nicolas',
+//         edad: 31,
+//         url: req.url
+//     };
+//     res.send(salida);
+// });
+// app.get('/data', function (req, res) {
+//     res.send('Hello Data')
+//     // let salida = {
+//     //     nombre: 'Nicolas',
+//     //     edad: 31,
+//     //     url: req.url
+//     // };
+//     // res.send(salida);
+// });
 
 app.listen(3000, ()=> {
     console.log('Escuchando peticiones del puerto 3000')
